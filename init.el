c43a231f0ac966cec5973b5a23eb18005e52eaf7
Tim Humphries <tim@utf8.me>
2017-07-12 12:59:46 +0900

    Initial commit

---
+++ init.el
@@ -0,0 +1,36 @@
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
+(cua-mode 't)
+(electric-indent-mode 0)
+(load-theme 'tango-dark)
+
+;; CUA mode for selection / mutation / etc.
+(cua-mode t)
+(setq cua-auto-tabify-rectangles nil) ;; Don't tabify after rectangle commands
+(transient-mark-mode 1) ;; No region when it is not highlighted
+(setq cua-keep-region-after-copy t) ;; Standard Windows behaviour
+(global-set-key (kbd "C-x C-v") 'cua-set-rectangle-mark)
+
+(show-paren-mode 1)
+
+(global-linum-mode 1)
+(setq linum-format "%4d ") ; Default formatting has no spacing
+
+(add-hook 'prog-mode-hook (lambda () (setq show-trailing-whitespace t)))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (haskell-mode dracula-theme))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
